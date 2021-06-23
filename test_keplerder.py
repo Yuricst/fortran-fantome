fix precision to wp::float64

test_keplerder.py
@@ -9,15 +9,16 @@ python -m numpy.f2py -c linalg.f90 orbitalelements.f90 keplerder.f90 -m keplerde
 ```
 """
 
+import time
 from keplerder import keplerder
 
 mu = 1.0
-state0 = [ 1.0, 0.0, 0.2, 0.0, 0.98, 0.067 ]
+state0 = [ 1.01, 0.0, 0.2, 0.0, 0.98, 0.067 ]
 t0 = 0.0
 t = 3.14
 
-tol = 1.e-13
-maxiter = 10
+tol = 1.e-14
+maxiter = 12
 
 state1 = keplerder.propagate(mu, state0, t0, t, tol, maxiter)
 print("state0: ")
@@ -25,3 +26,17 @@ print(state0)
 print("state1: ")
 print(state1)
 
+
+# for comparison
+import sys
+sys.path.append(r"C:\Users\yurio\Documents\GitHub\galt")
+import pygalt
+import numpy as np
+
+state2 = pygalt.keplerder_nostm(mu, np.array(state0), t0, t, tol=tol, maxiter=maxiter)
+print("state2: ")
+print(state2)
+
+
+# measure time
+tstart = time.time()